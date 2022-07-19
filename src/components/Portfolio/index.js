8493c7e9693d789b3fe0b99094ee96c7e835b584
MNlegion <rkreuser30@gmail.com>
2022-07-18 21:47:12 -0500

    working through bugs and errors when attepmting to compile the app

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,6 @@
 import React from 'react'; 
-// import ProjectCards from '../../components/Project' - NEED TO CREATE THIS
-// import portfolio from portfolio.json - NEED to CREATE THIS PAGE WITH PORT PROJECTS
+import ProjectCards from '../../components/Project'
+// import portfolio from '../../portfolio.json'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Wrapper(props) {
@@ -17,7 +17,7 @@ function Portfolio () {
           </div>
 
           <Wrapper id="card-data">
-            {portfolio.map((project) => (
+            {Portfolio.map((project) => (
               <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy} topics={project.topics}/>
             ))}
           </Wrapper>
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,1 +1,40 @@
 import React from 'react'; 
+
+function Resume() {
+
+return(
+<section class="mb-5">
+    <h1 className="resume">resume: Robert Kreuser</h1>
+        <hr></hr>
+        <div class="row justify-content-center" id="resume">
+            <div class="mt-5 pl-5 pr-5">
+                Business executive and full stack developer with experience the MERN and MEAN stacks. Experienced in commercial website and software development. Joseph is an experienced leader with a demonstrated history of working in oil and gas mineral rights and royalty acquisition and management. Throughout his time in the industry he has acquired and managed oil and gas interests in over 20,000 wells throughout 30 States. Joseph has experience in all aspects of oil and gas exploration, production, development and management. 
+
+                <p class="mt-5">
+                <a href="https://www.linkedin.com/in/robert-kreuser-b09bb7133/"><img src="https://img.icons8.com/color/48/000000/linkedin-2.png" alt="Rkreuser LinkedIn"/></a>
+                </p>
+                <p>
+                {/* Download my full <a href="NEED TO INSERT IN RESUME LINK FROM ASSETS" class="link">resume</a> */}
+                <br></br>
+                </p>
+            </div>
+        </div>
+
+        <div class="justify-content-center mt-5">
+            <div id="front-back">
+                <h2>front end experience</h2>
+                <p>HTML, CSS (Bootstrap, Foundation, Bulma), JavaScript, jQuery, React.js, Angular.js, IndexedDB</p>
+            </div>
+    
+            <div id="front-back" class="mt-5">
+                <h2>back end experience</h2>
+                <p>
+                Node.js, Express.js, SQL (sqlite, mySQL, postgreSQL), Sequelize, NoSQL (MongoDB, Mongoose), Python, Django, API's (third-party, RESTful, server-side), Templating (Handlebars)
+                </p>
+            </div>
+        </div>
+</section>
+);
+}
+
+export default Resume;
